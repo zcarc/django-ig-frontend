6bf5ea50c9a99bab20df74dab5957f095c1818eb
wrkbr <zcarc1@gmail.com>
2020-02-12 07:04:13 +0900

    16. Event Delegation

--- js/main.js
+++ js/main.js
@@ -1,15 +1,50 @@
 
 // 하트아이콘의 부모를 가져오는게 더 효율적입니다.
-const heart = document.querySelector('.heart_btn');
+// const heart = document.querySelector('.heart_btn');
 
 const header = document.querySelector('#header');
 const sideBox = document.querySelector('.side_box');
 const variableWidth = document.querySelectorAll('.contents_box .contents');
+const delegation = document.querySelector('.contents_box');
 
-heart.addEventListener('click', function () {
-    console.log('hit');
-    heart.classList.toggle('on');
-});
+// heart.addEventListener('click', function () {
+//     console.log('hit');
+//     heart.classList.toggle('on');
+// });
+
+function delegationFunc(e) {
+    console.log(e.target);
+
+    let element = e.target;
+
+    while(!element.getAttribute('data-name')) { // null
+        element = element.parentNode;
+        console.log('element.parentNode: ', element);
+
+        if(element.nodeName === 'BODY') {
+            console.log('element.nodeName: ', element.nodeName);
+            element = null;
+            return;
+        }
+    }
+
+    if(element.matches('[data-name="heartbeat"]')) {
+        console.log('하트!');
+
+    } else if(element.matches('[data-name="bookmark"]')) {
+        console.log('북마크!');
+
+    } else if(element.matches('[data-name="share"]')) {
+        console.log('공유!');
+
+    } else if(element.matches('[data-name="more"]')) {
+        console.log('더보기!');
+    }
+
+    element.classList.toggle('on');
+
+    console.log('END delegationFunc...');
+}
 
 function resizeFunc() {
     console.log('resize!');
@@ -37,8 +72,12 @@ function resizeFunc() {
         // }
 
     } else {
+
         variableWidth.forEach((element, index) => {
-            element.removeAttribute('style');
+
+            if(window.innerWidth > 600) {
+                element.removeAttribute('style');
+            }
         });
     }
 
@@ -48,15 +87,30 @@ function scrollFunc() {
     console.log(pageYOffset);
 
     if(pageYOffset >= 10) {
+
         header.classList.add('on');
-        sideBox.classList.add('on');
+
+        // 다른 html 파일에서 main.js를 사용해도 에러가 발생하지 않는다.
+        if(sideBox) {
+            sideBox.classList.add('on');
+        }
+
         resizeFunc();
     } else {
         header.classList.remove('on');
-        sideBox.classList.remove('on');
-        sideBox.removeAttribute('style');
+
+        if(sideBox) {
+            sideBox.classList.remove('on');
+            sideBox.removeAttribute('style');
+        }
+
     }
 }
 
+setTimeout(function () {
+    scrollTo(0, 0);
+}, 100);
+
+delegation.addEventListener('click', delegationFunc);
 window.addEventListener('resize', resizeFunc);
 window.addEventListener('scroll', scrollFunc);
